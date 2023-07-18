7f1b989e9a1ca6e1be885f36984b0626db6af4f3
Daoul <lancinediak@gmail.com>
2023-07-18 19:43:45 +0200

    dev

--- actividad6.js
+++ actividad6.js
@@ -2,29 +2,37 @@
 // .Pedir clave
 // .Si clave igual a 123456
 // .Dar acceso
-// .Si no
+// .Sino
 // .Pedir clave
-// .No dar más de tres
-// .
-// .
+// .No pedir más de tres veces la clave
+// .Mensaje "la clave es incorrecta"
+// .Finsi
 // .
 // .
 
 // FinProceso
 
-var clave = parseInt(prompt("Introduce la clave: " ));
-    console.log("La clave: "  + clave);
+// var clave = parseInt(prompt("Introduce la clave: " ));
+    // console.log("La clave: "  + clave);
 
-for ( var i=1; i<=3; i++){
-    if(clave == 123456){
-    document.write(" Dar acceso");
-    break;
-    }
-    else {
-        var clave = parseInt(prompt("Introduce la clave: " ));
+// for ( var i=1; i<3; i++){
+//     if(clave == 123456){
+//     document.write(" Acceso ");
+//     break;
+//     }
+//     else {
+//         document.write(" la clave es incorrecta ");
+//         var clave = parseInt(prompt("Introduce la clave: " ));
+           
+//     }
+// }
+for(var i=0;i<3; i++){
+    var contraseña = prompt("Introduce la contraseña");
 
-        // document.write(" la clave es incorrecta");
-    
+    if(contraseña=="123456"){
+        document.write("Es correcto");
+        break;
     }
 }
+document.write("No es correcto");
 
