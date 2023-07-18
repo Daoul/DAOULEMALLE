1d32a2d1820795438f86ffa721e6ba6fdae71caf
Daoul <lancinediak@gmail.com>
2023-07-18 18:24:39 +0200

    dev

---
+++ actividad5.js
@@ -0,0 +1,4 @@
+var importe = parseInt(prompt("El importe de la compra: " ));
+var mes = (prompt("Introduce el mes: "));
+    console.log("El importe de la compra es: "  + importe);
+    console.log("El mes es: "  + mes);
