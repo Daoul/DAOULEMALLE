c26a0d761168d7d7cb92bb8e58760f1b3dcdaf5c
Daoul <lancinediak@gmail.com>
2023-07-20 18:52:18 +0200

    dev

--- actividad10.js
+++ actividad10.js
@@ -39,33 +39,31 @@ document.write( "El total es " + importe + " €" );
 
 }
 
-// var importe = parseInt(prompt("El importe de la compra: " ));
-// var mes = (prompt("Introduce el mes: "));
-// var pfinal = 0;
-// document.write("El importe de la compra es: "  + importe + "€" +"<br/>");
-// console.log("El mes es: "  + mes +"<br/>");
+//Actividad10
 
-// switch (mes) 
+// var precio = parseInt(prompt("Escribe el importe de la compra"));
+// var mes = prompt("Introduce el mes");
+// var pfinal= 0;
+// document.write("El precio es  "+precio + " €"+"<br/>");
+// document.write("El mes es "+mes +"<br/>");
 
-// {
-//     // enero y febrero un 20%.
-//     // case enero || febrero:
-//     case enero:
-//         total = importe * 0.80;
-//     break;
-//     case febrero:
-//         total = importe * 0.85;
-//     break;
-//     // Julio y agosto un 15%.
-//     // case julio || agosto:
-//     //     total = importe * 0.85;
-//     // break;
-//     // // Noviembre y diciembre un 25%.
-//     // case Noviembre || diciembre:
-//     //     total = importe * 0.75;
-//     // break;
+// mes = (mes.toLowerCase());
+// switch(mes) {
+//     case "enero": 
+//     case "febrero":
+//         pfinal = precio*0.80;
+//         document.write("El precio final es "+pfinal +"<br/>");
+//         break;
+//     case "julio": case "agosto":
+//         pfinal = precio*0.85;
+//         document.write("El precio final es "+pfinal +"<br/>");
+//         break;
+//     case "noviembre": case "diciembre":
+//         pfinal = precio*0.75;
+//         document.write("El precio final es "+pfinal +"<br/>");
+//         break;
 //     default:
-//         total = importe;
-//     break;
+//         pfinal = precio;
+//         document.write("El precio final es "+pfinal +"<br/>");
+//         break;
 // }
-// console.log(total);
