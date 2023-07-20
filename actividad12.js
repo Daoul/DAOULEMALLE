c7f2377d0d354f0fea676a25240429798e3ed6d4
Daoul <lancinediak@gmail.com>
2023-07-20 18:09:55 +0200

    dev

---
+++ actividad12.js
@@ -0,0 +1,12 @@
+let operador = prompt("operadores");
+resultado = eval(operador);
+console.log(resultado);
+
+
+function comprobar(){
+
+    let operador = document.getElementById("operadores").value;
+    resultado = eval(operador);
+    document.getElementById("resultado").innerHTML="Resultado: " + resultado;
+    
+}
