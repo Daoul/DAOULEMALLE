d5e3fe77fb9e0db12fc954fb095554317be905e1
Daoul <lancinediak@gmail.com>
2023-07-20 17:01:49 +0200

    dev

--- actividad11.js
+++ actividad11.js
@@ -3,18 +3,23 @@ var identi = prompt("¿Quien es? " );
 if (identi == "admin" || identi == "Admin" || identi == "ADMIN" ){
     var contrasena = prompt("Introduce la contarseña ");
     if(contrasena == "webmaster"){
-        console.log("Bienvenido!");
+        document.write( "Bienvenido"+ "<br/>");
+        // console.log("Bienvenido!");
     }
     else if(contrasena == "" || contrasena == null){
-        console.log("Cancelado");
+        document.write( "Cancelado"+ "<br/>");
+        // console.log("Cancelado");
     }
     else{
-        console.log("Incorrecto");
+        document.write( "Incorrecto"+ "<br/>");
+        // console.log("Incorrecto");
     }
 }
 else if(identi == "" || identi == null){
-    console.log("Cancelado");
+    document.write( "Cancelado"+ "<br/>");
+    // console.log("Cancelado");
 }
 else{
-    console.log("Incorrecto");
+    document.write( "Incorrecto"+ "<br/>");
+    // console.log("Incorrecto");
 }
