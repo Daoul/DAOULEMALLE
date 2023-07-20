7ac4acad08d0a933dd883d5c5f152f1d2d98bead
Daoul <lancinediak@gmail.com>
2023-07-20 20:08:38 +0200

    dev

--- actividad2.js
+++ actividad2.js
@@ -1,10 +1,36 @@
-var a = parseInt(prompt("El primer número: " ));
-var b = Number(prompt("El segundo número: "));
-    console.log(a);
-    console.log(b);
+// var a = parseInt(prompt("El primer número: " ));
+// var b = Number(prompt("El segundo número: "));
+//     console.log(a);
+//     console.log(b);
 
-    console.log(a+b);
-    document.write(a+b + "<br/>");
-    document.write(a-b + "<br/>");
-    document.write(a*b + "<br/>");
-    document.write(a/b + "<br/>");
+//     console.log(a+b);
+//     document.write(a+b + "<br/>");
+//     document.write(a-b + "<br/>");
+//     document.write(a*b + "<br/>");
+//     document.write(a/b + "<br/>");
+
+// Actividad2: Correction de Manuel
+// var a = Number(prompt("Introduce el primer número"));
+// console.log("El primer número es " + a);
+// var b = Number(prompt("Introduce el segundo número"));
+// console.log("El segundo número es " + b);
+
+// document.write("Sumar: " + (a+b) + "<br/>");
+// document.write("Restar: " + (a-b) + "<br/>");
+// document.write("Multiplicar: " + (a*b) + "<br/>");
+// document.write("Dividir: " + (a/b) + "<br/>");
+
+function calcular(){
+
+    var num1 = Number(document.getElementById("numero1").value);
+    var num2 = Number(document.getElementById("numero2").value);
+    sumar = num1 + num2;
+    restar = num1 - num2;
+    multiplicar = num1 * num2;
+    dividir = num1 / num2;
+    document.getElementById("sumar").innerHTML="Sumar: " + sumar;
+    document.getElementById("restar").innerHTML="Restar: " + restar;
+    document.getElementById("multiplicar").innerHTML="multiplicar: " + multiplicar;
+    document.getElementById("dividir").innerHTML="dividir: " + dividir;
+    
+}
--- actividad3.js
+++ actividad3.js
@@ -14,9 +14,31 @@ var b = parseInt(prompt("El segundo número: "));
             console.log("El segundo número es major " );
             }
 
-           
-    
+  //Actividad3: Correction de Manuel
 
+// var a = parseInt(prompt("Introduce el primer número"));
+// console.log("El primer número es " + a);
+// var b = parseInt(prompt("Introduce el segundo número"));
+// console.log("El segundo número es " + b);
+// if (a==b) {
+//     console.log("El número " + a + " y " + "el número " + b + " son iguales");
+// }else if (a>b){
+//     console.log("El primero es mayor.")
+// } else if (b>a){
+//     console.log("El segundo es mayor")
+// }else{
+//     console.log("Algo pasa!!!");
+// }          
+
+    
+// function calcular(){
+
+//     var num1 = Number(document.getElementById("numero1").value);
+//     var num2 = Number(document.getElementById("numero2").value);
+//     sumar = num1 + num2;
+    
+//     document.getElementById("sumar").innerHTML="Sumar: " + sumar;
+// }
         
 
     
